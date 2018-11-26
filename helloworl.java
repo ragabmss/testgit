0966cb9ae1848cf57d9139412a254f90196e2ff4
ragabmss <ragabms@gmail.com>
2018-11-26 19:14:31 +0800

    additions committed

--- helloworl.java
+++ helloworl.java
@@ -1,5 +1,6 @@
 public class helloworl{
 public static void main(String[] args) {
-System.out.println("Hello World for Gitcommit");
+System.out.println("Hello World for Gitcommit added new lines to commit");
+System.out.println("Added new lines to commit");
 }
 }
